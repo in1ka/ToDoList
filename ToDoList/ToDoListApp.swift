9756ef770872c97f7bc271d213de40ff1a46daa6
Inika Bhargava <inikamehro@gmail.com>
2025-05-04 19:55:41 -0700

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Inika Bhargava on 5/4/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
